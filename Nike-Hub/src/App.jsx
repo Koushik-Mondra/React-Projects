839338995c5bd59d5f0fe5327bf4a6deacb721f7
Koushik-Mondra <koushik17x@gmail.com>
2024-06-16 13:13:38 +0530

    first commit

---
+++ Nike-Hub/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navigation from './assets/components/Navigation';
+import Herosection from './assets/components/Hero';
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Herosection/>
+    </div>
+  )
+};
+
+export default App;
